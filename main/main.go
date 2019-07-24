a71f1e932ccbf8e8b0a192d094ef6270940b21bb
PhilDev22 <agameamonthblog@gmail.com>
2019-07-24 16:05:45 +0200

    Moved enahce-ads functions form gui_script to ads.script

--- main/main.go
+++ main/main.go
@@ -43,6 +43,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "ads"
+  component: "/main/ads.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite_background"
   type: "sprite"
